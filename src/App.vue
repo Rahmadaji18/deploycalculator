5e2548d600ea62eb1dfa106aadef88746b4bd8ed
Rahmad Aji <aji.wicaksono18@gmail.com>
2023-11-05 23:16:37 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app-container">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <CalculatorApp></CalculatorApp>
+  </div>
+</template>
+
+<script>
+import CalculatorApp from './components/CalculatorApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorApp
+  }
+}
+</script>
+
+<style>
+  body {
+    background: #31475e;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .app-container {
+    text-align: center;
+  }
+</style>
